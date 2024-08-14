9d99dff87533dcb3ff7a92a05f53224a78e8ae9c
Markus-Gerrits <markus.gerrits@gmail.com>
2024-08-13 20:59:10 -0400

    Create model Phothography

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    data = {
+        1: {"name": "Nebulosa de Carina",
+            "legend": "webbtelecope.org / NASA / James Webb"},
+        2: {"name": "Galáxia NGC 1079",
+            "legend": "nasa.org / NASA / Hubble"}
+    }
+    return render(request, 'galeria/index.html', {"cards": data})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria',
+    'galeria.apps.GaleriaConfig',
 ]
 
 MIDDLEWARE = [
@@ -114,7 +114,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
