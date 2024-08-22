a078f3d9734c4ef66991b034a15a3419eff69400
Markus-Gerrits <markus.gerrits@gmail.com>
2024-08-21 21:37:59 -0400

    Add ImageField in model and change how to show image

--- galeria/models.py
+++ galeria/models.py
@@ -14,9 +14,9 @@ class Phothography(models.Model):
     legend = models.CharField(max_length=150, null=False, blank=False)
     category = models.CharField(max_length=100, choices=CATEGORY_OPTIONS, default='')
     description = models.TextField(null=False, blank=False)
-    photo = models.CharField(max_length=150, null=False, blank=False)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
     published = models.BooleanField(default=False)
     phothography_date = models.DateTimeField(default=datetime.now, blank=False)
     
     def __str__(self):
-        return f"Photography [name={self.name}]"
+        return self.name
