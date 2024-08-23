ff00cf7d3932af1236de78ef81e0e896d9af9fc2
Markus-Gerrits <markus.gerrits@gmail.com>
2024-08-22 21:19:42 -0400

    Add search feature

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, search
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/<int:photo_id>', imagem, name='imagem')
+    path('imagem/<int:photo_id>', imagem, name='imagem'),
+    path('search', search, name='search')
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -9,3 +9,13 @@ def index(request):
 def imagem(request, photo_id):
     photo = get_object_or_404(Phothography, pk=photo_id)
     return render(request, 'galeria/imagem.html', {'phothography': photo})
+
+def search(request):
+    photos = Phothography.objects.order_by("phothography_date").filter(published=True)
+    
+    if "search" in request.GET:
+        name_for_search = request.GET['search']
+        if name_for_search:
+            photos = photos.filter(name__icontains=name_for_search)
+    
+    return render(request, 'galeria/search.html', {"cards": photos})
